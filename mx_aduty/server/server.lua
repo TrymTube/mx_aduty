a2a983129f30f60e07d48a65e38c09920d00dd50
mx developement <62698394+TrymTube@users.noreply.github.com>
2022-08-11 05:44:59 +0200

    Delete mx_aduty directory

--- mx_aduty/server/server.lua
+++
@@ -1,9 +0,0 @@
-ESX = exports['es_extended']:getSharedObject()
-
-ESX.RegisterServerCallback('mx_aduty:getValues', function(source, cb)
-    local source = source
-    local xPlayer = ESX.GetPlayerFromId(source)
-    local playerGroup = xPlayer.getGroup()
-    
-    cb(playerGroup)
-end)
