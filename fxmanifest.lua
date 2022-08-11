0d31308f148afca326a9540224a8830e99cb102d
mx developement <62698394+TrymTube@users.noreply.github.com>
2022-08-11 05:45:13 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,22 @@
+fx_version 'bodacious'
+game 'gta5'
+
+author 'Max'
+description 'simple aduty system'
+version '0.1.0'
+
+lua54 'yes'
+
+shared_script {
+	'@es_extended/imports.lua',
+	'@es_extended/locale.lua',
+	'config.lua'
+}
+
+client_scripts {
+	'client/*.lua'
+}
+
+server_scripts {
+	'server/*.lua'
+}
